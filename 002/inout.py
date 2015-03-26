2e0e884ff6d18bc1c2ba740449028c9e395bce11
sauhor <sauhor@outlook.com>
2015-03-26 22:07:24 +0900

    First commit

--- 002/inout.py
+++ 002/inout.py
@@ -1,9 +1,15 @@
 # inout.py
 #coding: utf-8
 
+import sys
 from solve import solve
 
-f=open("problem.txt","r")
+argvs=sys.argv
+argc=len(argvs)
+if argc >= 2:
+    f=open(argvs[1],"r")
+else:
+    f=open("problem.txt","r")
 noshi=[line.strip('\n') for line in f]
 spam=noshi[0].split(' ')
 n=spam[0]
